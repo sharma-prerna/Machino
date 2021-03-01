7df45b31dfbd85d4988eb98791bbdcb12e16fd9e
Prerna Sharma <61116393+sharma-prerna@users.noreply.github.com>
2021-03-01 16:51:59 +0530

    Updated Models.py

--- django_ml_project/machino/models.py
+++ django_ml_project/machino/models.py
@@ -3,17 +3,4 @@ from os import listdir, getcwd
 from os.path import join
 # Create your models here.
 
-'''class Parameters(models.Model):
-	k = models.PositiveIntegerField()
-	split = models.PositiveIntegerField()
-	learning_rate = models.FloatField()
-	activation = (
-			("sigmoid","sigmoid"),
-			("relu","relu"),
-			("tanh","tanh"),
-		)
-	epochs = models.PositiveIntegerField()
-	layers = models.CharField(max_length=20)
-	def __str__(self):
-		return "new_dataset"'''
 
